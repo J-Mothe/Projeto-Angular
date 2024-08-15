a9ddf7c808027a7575dffcc4c60a5e69aaa345f7
joao.mothe <joao_mothe@hotmail.com>
2024-08-15 18:06:45 -0300

    refactor: refatora listagem de produtos

---
+++ src/app/shared/services/products.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Product } from '../interfaces/product.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  httpClient = inject(HttpClient);
+
+  getAll() {
+  return this.httpClient.get<Product[]>('/api/products');
+}
+
+}
