24b1450c9c886fe42e61eac2d85f2dbae88598e8
Joao Mothe <joao_mothe@hotmail.com>
2024-08-16 17:41:18 -0300

    feat: adiciona campos ao formulario de criação de produto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,16 @@ import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
 import { CreateComponent } from './features/create/create.component';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+  {
     path: '',
     component: ListComponent,
-
-},
-{
+  },
+  {
     path: 'create-product',
-    loadComponent: () =>  
-        import('./features/create/create.component').then(
-            (m) => m.CreateComponent
-        ),
-    },
+    loadComponent: () =>
+      import('./features/create/create.component').then(
+        (m) => m.CreateComponent
+      ),
+  },
 ];
--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,12 +1,32 @@
+import { NonNullAssert } from '@angular/compiler';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { title } from 'process';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-create',
+  selector: 'app-create', 
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './create.component.html',
-  styleUrl: './create.component.scss'
+  styleUrl: './create.component.scss',
 })
 export class CreateComponent {
+  form = new FormGroup({ 
+    title: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+  });
 
+  onSubmit() {
+    this.form.controls.title.value;
+  }
 }
