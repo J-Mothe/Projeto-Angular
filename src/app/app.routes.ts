cdba4519c7e8418093e0ab0c8a2fd17e330b6026
Joao Mothe <joao_mothe@hotmail.com>
2024-08-21 17:36:11 -0300

    refactor: Melhora codigo de ListComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,12 @@ import { ProductsService } from './shared/services/products.service';
 export const routes: Routes = [
   {
     path: '',
+    resolve: {
+      products: () => {
+        const productsService = inject(ProductsService);
+        return productsService.getAll();
+      },
+    },
     component: ListComponent,
   },
   {
--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,16 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
-import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { CardComponent } from './components/card/card.component';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import {
   MatDialog,
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogRef,
-  MatDialogTitle,
 } from '@angular/material/dialog';
 import { filter } from 'rxjs';
 import { ConfirmationDialogService } from '../../shared/services/confirmation-dialog.service';
@@ -23,18 +18,14 @@ import { ConfirmationDialogService } from '../../shared/services/confirmation-di
   styleUrl: './list.component.scss',
 })
 export class ListComponent {
-  products: Product[] = [];
+  products = signal<Product[]>(
+    inject(ActivatedRoute).snapshot.data['products']
+  );
 
   productsService = inject(ProductsService);
   router = inject(Router);
   matDialog = inject(MatDialog);
   confirmationDialogService = inject(ConfirmationDialogService);
-
-  ngOnInit() {
-    this.productsService.getAll().subscribe((products) => {
-      this.products = products;
-    });
-  }
 
   onEdit(product: Product) {
     this.router.navigate(['/edit-product', product.id]);
@@ -46,7 +37,7 @@ export class ListComponent {
       .subscribe(() => {
         this.productsService.delete(product.id).subscribe(() => {
           this.productsService.getAll().subscribe((products) => {
-            this.products = products;
+            this.products.set(products);
           });
         });
       });
